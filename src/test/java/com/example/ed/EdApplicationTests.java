241a32ecae8603a2344bfa731adc3534a2f8dff7
Sardor Madatov <sardormadatov04@gmail.com>
2026-09-14 10:42:19 +0500

    initial

---
+++ src/test/java/com/example/ed/EdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ed;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EdApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
